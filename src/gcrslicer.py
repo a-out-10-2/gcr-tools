e965c41a0736b7152e51bace192a237d5eac942a
a_out_10_2 <moe.andrew@gmail.com>
2022-10-23 02:55:12 -0500

    Moved and renamed script. Added FilePathsIterator and GCRArgumentParser (for catchable syntax exceptions).

--- src/gcrslicer.py
+++ src/gcrslicer.py
@@ -17,6 +17,33 @@ __version__ = 0.0
 __all__ = ['main', 'parse_args']
 
 
+class FilePathsIterator:
+	FOLLOWLINKS = True
+	SUPPORTED_READ_EXTENSIONS = {'wav', 'flac'}
+	SUPPORTED_WRITE_EXTENSIONS = {'wav', 'aiff', 'aifc'}
+
+	def __init__(self, audio_file_or_dir_paths):
+		self.in_paths = audio_file_or_dir_paths
+
+	def __next__(self):
+		path_obj = Path(self.in_paths.pop(0))
+		return path_obj
+
+	def __iter__(self):
+		return self
+
+
+class GCRArgumentParser(argparse.ArgumentParser):
+	def __init__(self, description):
+		super().__init__(description=description, exit_on_error=False)
+
+	def exit(self, status=0, message=None):
+		if status:
+			raise argparse.ArgumentError(argument=None, message="(status: {}, message: '{}'".format(status, message))
+
+	def error(self, message):
+		raise argparse.ArgumentError(argument=None, message=message)
+
 def parse_args(*args, **kwargs):
 	"""Parse the arguments received from STDIN.
 	param args: The string arguments to be parsed.
@@ -24,7 +51,7 @@ def parse_args(*args, **kwargs):
 	rtype: argparse.Namespace
 	"""
 	# Constructing argument parser
-	parser = argparse.ArgumentParser(description="* GCR Slicer |/-\\")
+	parser = GCRArgumentParser(description="* GCR Slicer |/-\\")
 	parser.add_argument("audio_file_or_dir_paths", type=str, nargs='+', help="A number of positional paths.")
 	mutux = parser.add_mutually_exclusive_group()
 	mutux.required = True  # Require a mutux option
@@ -34,7 +61,13 @@ def parse_args(*args, **kwargs):
 	parser.add_argument("--version", action='version', version='cli %s' % __version__)
 
 	# Process and return parameters
-	return parser.parse_args(*args, **kwargs)
+	try:
+		params = parser.parse_args(*args, **kwargs)
+	except argparse.ArgumentError as ae:
+		print(f"ArgumentError: {ae}", file=sys.stderr)
+		params = None
+
+	return params
 
 
 def main(params):
@@ -58,15 +91,23 @@ def main(params):
 	exec_cwd = os.getcwd()
 
 	# Read input positionals and resolve them to a list/iterator of filepaths.
-	for p in params.audio_file_or_dir_paths:
-		path_obj = Path(p)
-		logging.debug("DBG: path_obj.name\t\t= {}".format(path_obj.name))
-		logging.debug("DBG: path_obj.resolve() = {}".format(path_obj.resolve()))
-		logging.debug("DBG: path_obj.is_dir()\t= {}".format(path_obj.is_dir()))
-		logging.debug("DBG: path_obj.is_file()\t= {}".format(path_obj.is_file()))
+	# for p in params.audio_file_or_dir_paths:
+	# 	path_obj = Path(p)
+	# 	logging.debug("DBG: path_obj.name\t\t= {}".format(path_obj.name))
+	# 	logging.debug("DBG: path_obj.resolve() = {}".format(path_obj.resolve()))
+	# 	logging.debug("DBG: path_obj.is_dir()\t= {}".format(path_obj.is_dir()))
+	# 	logging.debug("DBG: path_obj.is_file()\t= {}".format(path_obj.is_file()))
+	fpi = FilePathsIterator(params.audio_file_or_dir_paths)
+
+	logging.info("BREAK HERE!")
 
 	return 0
 
 
 if __name__ == '__main__':
-	sys.exit(main(parse_args()))
+	print("START HERE")
+	args = sys.argv[1:]
+	params = parse_args(args)
+	print("BREAK!")
+	# rc = main(params)
+	# sys.exit(rc)
