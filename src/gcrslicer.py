94ea6059fc6db1fa407dc2224cbcd80a6584b75c
a_out_10_2 <moe.andrew@gmail.com>
2022-10-23 15:29:51 -0500

    Made progress with FilePathsIterator.

--- src/gcrslicer.py
+++ src/gcrslicer.py
@@ -15,26 +15,76 @@ import webrtcvad
 __version__ = 0.0
 
 # Exportable API
-__all__ = ['main', 'parse_args']
+# __all__ = ['main', 'parse_args']
 
 
 class FilePathsIterator:
-	FOLLOWLINKS = True
 	SUPPORTED_READ_EXTENSIONS = {'wav', 'flac'}
 	SUPPORTED_WRITE_EXTENSIONS = {'wav', 'aiff', 'aifc'}
 
-	def __init__(self, audio_file_or_dir_paths):
+	OSWALKER_TOPDOWN = True
+	OSWALKER_FUNC_ON_ERROR = None  # set func for Errors
+	OSWALKER_FOLLOWLINKS = True
+
+	def __init__(self, audio_file_or_dir_paths, top=os.getcwd()):
 		self.in_paths = audio_file_or_dir_paths
+		self.top = top
+
+		self.current_filepath = None
+		self.current_walker = None
 
 	def __next__(self):
 		""":return: Return a new pathlib.Path object of next file
+		:raises: StopIteration to signal iterator's emptiness.
 		"""
-		path_obj = None
+
+		# Attempt to resolve the next positional path.
+		current_positional = None
 		try:
-			path_obj = Path(self.in_paths.pop(0))
-		except IndexError:
+			# TODO: If resuming from previous os.walk, don't pop a pos
+			current_positional = self.__pop_next_existing_positional_path()
+		except StopIteration:
+			logging.debug(f"caught StopIteration during __pop_next_positional_path(), raising StopIteration")
 			raise StopIteration
-		return path_obj
+
+		logging.debug(f"SUCCESS! positional '{current_positional}' has been popped!")
+		logging.debug(f"new_path='{current_positional}'")
+		logging.debug(f"\texists?='{current_positional.exists()}'")
+		logging.debug(f"\tis_dir?='{current_positional.is_dir()}'")
+		logging.debug(f"\tis_file?='{current_positional.is_file()}'")
+
+		if current_positional.is_dir():
+			current_dir_scope = os.path.join(self.top, current_positional.name)
+			self.current_walker = os.walk(current_dir_scope, topdown=self.OSWALKER_TOPDOWN, onerror=self.OSWALKER_FUNC_ON_ERROR, followlinks=self.OSWALKER_FOLLOWLINKS)
+			# TODO: Walk os.dir() until the first file is found, return it
+			root, dirs, files = self.current_walker.__next__()
+			logging.debug(f"OS_WALKER START (root='{root}', dirs={dirs}, files={files})")
+			# OS_WALKER START (root='C:\Users\Gifty\PycharmProjects\gcr-tools\data', dirs=[], files=['21 Tom Cat, Individual Meows.flac', '34 Tom Cat, Meowing.flac', '35 Tom Cat, Purring.flac', '46 Tom Cat Growling.flac', '49 Tom Cat Hiss.flac'])
+			# OS_WALKER START (root='C:\Users\Gifty\PycharmProjects\gcr-tools\data2', dirs=[], files=['somefile.blr'])
+			pass
+
+		#
+		# if current_positional.is_file():
+		# 	self.current_filepath = current_positional
+		# 	# TODO: Cross-check file exentions agains list, skip if not on it
+
+		return current_positional
+
+	def __pop_next_existing_positional_path(self):
+		"""
+		:raises StopIteration:
+		:return:
+		"""
+		try:
+			new_path = Path(self.in_paths.pop(0))
+			while not new_path.exists():
+				logging.debug(f"new_path: '{new_path}', does not exist, popping next positional path")
+				new_path = Path(self.in_paths.pop(0))
+		except IndexError as ie:
+			logging.debug(f"caught IndexError ie:'{ie}' when popping positional, raising StopIteration")
+			raise StopIteration
+
+		return new_path
 
 	def __iter__(self):
 		return self
@@ -104,10 +154,8 @@ def main(params):
 		logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 
 	# DBG
-	exec_cwd = os.getcwd()
 	logging.debug(f"params: {params}")
 	logging.debug(f"webrtcvad: {dir(webrtcvad)}")
-	logging.debug(f"os.getcwd(): '{exec_cwd}'")
 
 	# Read input positionals and resolve them to a list/iterator of filepaths.
 	# for p in params.audio_file_or_dir_paths:
@@ -116,13 +164,10 @@ def main(params):
 	# 	logging.debug("DBG: path_obj.resolve() = {}".format(path_obj.resolve()))
 	# 	logging.debug("DBG: path_obj.is_dir()\t= {}".format(path_obj.is_dir()))
 	# 	logging.debug("DBG: path_obj.is_file()\t= {}".format(path_obj.is_file()))
+
 	fpi = FilePathsIterator(params.audio_file_or_dir_paths)
-
 	filepaths = [filepath for filepath in fpi]
-
-	logging.info(f"Discovered file paths: {filepaths}")
-	# for filepath in fpi:
-	# 	logging.info(f"\tfilepath={filepath}")
+	logging.info(f"**FINAL**: Discovered/Resolved file paths: {filepaths}")
 
 	return 0
 
