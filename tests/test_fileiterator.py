0c511c232c416b744f92adf5c53a73fb22df12c4
a_out_10_2 <moe.andrew@gmail.com>
2022-10-30 09:46:30 -0500

    Continued progress on FileIterator unit tests.

--- tests/test_fileiterator.py
+++ tests/test_fileiterator.py
@@ -6,13 +6,37 @@ from gcrslicer import FileIterator
 
 class TestGCRFileIterator(unittest.TestCase):
 
-	def test_something(self):
-		expected_filelist = [Path(), Path()]
+	def test_empty(self):
 		input_path_list = []  # must be strings
-		fpi = FileIterator(input_path_list)
-		filepaths = [filepath for filepath in fpi]
+		expected_filelist = []
 
-		self.assertListEqual(filepaths, expected_filelist)
+		fpi = FileIterator(input_path_list)
+		found_filepaths = [filepath for filepath in fpi]
+		self.assertListEqual(found_filepaths, expected_filelist, msg=f"Empty inputs (input_path_list:{input_path_list})"
+																	 f" into FileIterator did not yield an empty list "
+																	 f"(found_filepaths:{found_filepaths}).")
+
+	def test_file(self):
+		input_path_list = ['data2/anotherfile2.blr']  # must be strings
+		expected_filelist = [Path('data2/anotherfile2.blr')]
+
+		fpi = FileIterator(input_path_list)
+		found_filepaths = [filepath for filepath in fpi]
+		self.assertListEqual(found_filepaths, expected_filelist, msg=f"Inputs (input_path_list:{input_path_list})"
+																	 f" into FileIterator did not yield (found_filepa"
+																	 f"ths:{found_filepaths}) as expected (expected_"
+																	 f"filelist:{expected_filelist}).")
+
+	def test_dir(self):
+		input_path_list = ['data2/']  # must be strings
+		expected_filelist = [Path('data2/anotherfile2.blr'), Path('data2/somefile.blr')]
+
+		fpi = FileIterator(input_path_list)
+		found_filepaths = [filepath for filepath in fpi]
+		self.assertListEqual(found_filepaths, expected_filelist, msg=f"Inputs (input_path_list:{input_path_list})"
+																	 f" into FileIterator did not yield (found_filepa"
+																	 f"ths:{found_filepaths}) as expected (expected_"
+																	 f"filelist:{expected_filelist}).")
 
 
 if __name__ == '__main__':
