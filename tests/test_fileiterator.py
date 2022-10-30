c703f4246f31b97d50c7412c77f9ae5c72ecacaa
a_out_10_2 <moe.andrew@gmail.com>
2022-10-30 14:59:16 -0500

    Renamed some variables in FileIterator unit test.

--- tests/test_fileiterator.py
+++ tests/test_fileiterator.py
@@ -7,35 +7,36 @@ from gcrslicer import FileIterator
 class TestGCRFileIterator(unittest.TestCase):
 
 	def test_empty(self):
-		input_path_list = []  # must be strings
+		input_pathlist = []  # must be strings
 		expected_filelist = []
 
-		fpi = FileIterator(input_path_list)
-		found_filepaths = [filepath for filepath in fpi]
-		self.assertListEqual(found_filepaths, expected_filelist, msg=f"Empty inputs (input_path_list:{input_path_list})"
+		fpi = FileIterator(input_pathlist)
+		found_filelist = [filepath for filepath in fpi]
+		self.assertListEqual(found_filelist, expected_filelist, msg=f"Empty inputs (input_path_list:{input_pathlist})"
 																	 f" into FileIterator did not yield an empty list "
-																	 f"(found_filepaths:{found_filepaths}).")
+																	 f"(found_filelist:{found_filelist}).")
 
 	def test_file(self):
-		input_path_list = ['data2/anotherfile2.blr']  # must be strings
+		input_pathlist = ['data2/anotherfile2.blr']  # must be strings
 		expected_filelist = [Path('data2/anotherfile2.blr')]
 
-		fpi = FileIterator(input_path_list)
-		found_filepaths = [filepath for filepath in fpi]
-		self.assertListEqual(found_filepaths, expected_filelist, msg=f"Inputs (input_path_list:{input_path_list})"
-																	 f" into FileIterator did not yield (found_filepa"
-																	 f"ths:{found_filepaths}) as expected (expected_"
+		fpi = FileIterator(input_pathlist)
+		found_filelist = [filepath for filepath in fpi]
+		self.assertListEqual(found_filelist, expected_filelist, msg=f"Inputs (input_path_list:{input_pathlist})"
+																	 f" into FileIterator did not yield (found_filelis"
+																	 f"t:{found_filelist}) as expected (expected_"
 																	 f"filelist:{expected_filelist}).")
 
 	def test_dir(self):
-		input_path_list = ['data2/']  # must be strings
+		input_pathlist = ['data2/']  # must be strings
 		expected_filelist = [Path('data2/anotherfile2.blr'), Path('data2/somefile.blr')]
 
-		fpi = FileIterator(input_path_list)
-		found_filepaths = [filepath for filepath in fpi]
-		self.assertListEqual(found_filepaths, expected_filelist, msg=f"Inputs (input_path_list:{input_path_list})"
-																	 f" into FileIterator did not yield (found_filepa"
-																	 f"ths:{found_filepaths}) as expected (expected_"
+		fpi = FileIterator(input_pathlist)
+		found_filelist = [filepath for filepath in fpi]
+
+		self.assertListEqual(found_filelist, expected_filelist, msg=f"Inputs (input_path_list:{input_pathlist})"
+																	 f" into FileIterator did not yield (found_filelis"
+																	 f"t:{found_filelist}) as expected (expected_"
 																	 f"filelist:{expected_filelist}).")
 
 
