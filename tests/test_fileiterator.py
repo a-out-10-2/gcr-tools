8d9dc9d8280177b204d2d0a8f908c1338137bdb9
a_out_10_2 <moe.andrew@gmail.com>
2022-11-23 10:23:51 -0600

    Changed msg indentation. Added test for deep directories like data3/.

--- tests/test_fileiterator.py
+++ tests/test_fileiterator.py
@@ -13,8 +13,8 @@ class TestGCRFileIterator(unittest.TestCase):
 		fpi = FileIterator(input_pathlist)
 		found_filelist = [filepath for filepath in fpi]
 		self.assertListEqual(found_filelist, expected_filelist, msg=f"Empty inputs (input_path_list:{input_pathlist})"
-																	 f" into FileIterator did not yield an empty list "
-																	 f"(found_filelist:{found_filelist}).")
+																	f" into FileIterator did not yield an empty list "
+																	f"(found_filelist:{found_filelist}).")
 
 	def test_file(self):
 		input_pathlist = ['data2/anotherfile2.blr']  # must be strings
@@ -23,9 +23,9 @@ class TestGCRFileIterator(unittest.TestCase):
 		fpi = FileIterator(input_pathlist)
 		found_filelist = [filepath for filepath in fpi]
 		self.assertListEqual(found_filelist, expected_filelist, msg=f"Inputs (input_path_list:{input_pathlist})"
-																	 f" into FileIterator did not yield (found_filelis"
-																	 f"t:{found_filelist}) as expected (expected_"
-																	 f"filelist:{expected_filelist}).")
+																	f" into FileIterator did not yield (found_filelis"
+																	f"t:{found_filelist}) as expected (expected_"
+																	f"filelist:{expected_filelist}).")
 
 	def test_dir(self):
 		input_pathlist = ['data2/']  # must be strings
@@ -35,10 +35,25 @@ class TestGCRFileIterator(unittest.TestCase):
 		found_filelist = [filepath for filepath in fpi]
 
 		self.assertListEqual(found_filelist, expected_filelist, msg=f"Inputs (input_path_list:{input_pathlist})"
-																	 f" into FileIterator did not yield (found_filelis"
-																	 f"t:{found_filelist}) as expected (expected_"
-																	 f"filelist:{expected_filelist}).")
+																	f" into FileIterator did not yield (found_filelis"
+																	f"t:{found_filelist}) as expected (expected_"
+																	f"filelist:{expected_filelist}).")
 
+	def test_deep_dir(self):
+		input_pathlist = ['data3/']  # must be strings
+		expected_filelist = [Path('data3/oldaudio.mp1'),
+							 Path('data3/gabbaghoul/freshmeat.eat'),
+							 Path('data3/gabbaghoul/rottenm-eats.tar.xz'),
+							 Path('data3/gabbaghoul/littlepeppercorns/thatonethere.gps3'),
+							 Path('data3/gabbaghoul/littlepeppercorns/thisonehere.gps')]
+
+		fpi = FileIterator(input_pathlist)
+		found_filelist = [filepath for filepath in fpi]
+
+		self.assertListEqual(found_filelist, expected_filelist, msg=f"Inputs (input_path_list:{input_pathlist})"
+																	f" into FileIterator did not yield (found_filelis"
+																	f"t:{found_filelist}) as expected (expected_"
+																	f"filelist:{expected_filelist}).")
 
 	def test_file_dir(self):
 		input_pathlist = ['data3/oldaudio.mp1', 'data2/']  # must be strings
@@ -48,9 +63,9 @@ class TestGCRFileIterator(unittest.TestCase):
 		found_filelist = [filepath for filepath in fpi]
 
 		self.assertListEqual(found_filelist, expected_filelist, msg=f"Inputs (input_path_list:{input_pathlist})"
-																	 f" into FileIterator did not yield (found_filelis"
-																	 f"t:{found_filelist}) as expected (expected_"
-																	 f"filelist:{expected_filelist}).")
+																	f" into FileIterator did not yield (found_filelis"
+																	f"t:{found_filelist}) as expected (expected_"
+																	f"filelist:{expected_filelist}).")
 
 	def test_dir_file(self):
 		input_pathlist = ['data2/', 'data3/oldaudio.mp1']  # must be strings
